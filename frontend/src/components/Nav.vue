2adcef6d3195352cb8c9e7df04e20b70f9d675a8
LukaBrodaric <brodaricluka81@gmail.com>
2024-06-11 17:36:11 +0200

    navigacija router

--- frontend/src/components/Nav.vue
+++ frontend/src/components/Nav.vue
@@ -1,7 +1,49 @@
 <script setup>
-
+import { ref } from 'vue';
+const isOpened = ref(true);
+function toggleMenu() {
+    isOpened.value = !isOpened.value;
+};
 </script>
 
-<template>
 
+
+<template>
+    <div class="bg-slate-800 text-white">
+        <nav class="container mx-auto px-4">
+            <div class="hidden md:block">
+                <ul v-if="session" class="flex flex-row gap-4 justify-end py-4">
+                    <li><router-link to="/">Home</router-link></li>
+                    <li><router-link to="/file-upload">File Upload</router-link></li>
+                    <li><router-link to="/SviKolegiji">Kolegiji</router-link></li>
+                    <li><button @click="logout">Logout</button></li>
+                </ul>
+                <ul v-else class="flex flex-row gap-4 justify-start py-4">
+                    <li><router-link to="/">Sustav za upravljanje kolegijima</router-link></li>
+                </ul>
+            </div>
+
+            <div class="flex md:hidden p-4 justify-end">
+                <div>
+                    <button @click="toggleMenu" value="hamburger"
+                        class="flex flex-col justify-around h-8 w-8 p-2 rounded hover:bg-gray-400">
+                        <!-- Hamburger Icon (3 lines) -->
+                        <span class="h-0.5 rounded bg-gray-600 w-full"></span>
+                        <span class="h-0.5 rounded bg-gray-600 w-full"></span>
+                        <span class="h-0.5 rounded bg-gray-600 w-full"></span>
+                    </button>
+                </div>
+                <div v-if="isOpened"
+                    class="text-2xl z-10 absolute inset-0 bg-slate-200 flex flex-col gap-4 text-slate-900 text-center">
+                    <button @click="toggleMenu" class="bg-red-400 p-4 text-white">
+                        X
+                    </button>
+                    <router-link @click="toggleMenu" to="/">Home</router-link>
+                    <router-link @click="toggleMenu" to="/file-upload">File Upload</router-link>
+                    <router-link @click="toggleMenu" to="/SviKolegiji">Kolegiji</router-link>
+                    <button @click="logout">Logout</button>
+                </div>
+            </div>
+        </nav>
+    </div>
 </template>
